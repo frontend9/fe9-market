883c9ef40dd673f865cc23300361e543b2bb7895
fkysly <fkysly@gmail.com>
2018-11-14 16:18:25 +0800

    add banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,10 @@ import React from 'react'
 import styles from '../styles/components/Banner.module.less'
 
 const Banner = ({}) => (
-  <div className={styles.banner}>banner</div>
+  <div className={styles.banner}>
+    <h1 className={styles.title}>前端九部工具库</h1>
+    <p className={styles.slogan}>发现有价值的前端工具</p>
+  </div>
 )
 
 export default Banner
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Layout } from "antd";
-import { Link } from "react-router-dom";
+import { Layout } from 'antd';
+import { Link } from 'react-router-dom';
 import styles from '../styles/components/Header.module.less'
 import logo from '../assets/logo.png'
 
@@ -14,7 +14,7 @@ const HeaderCustom = ({}) => (
           <img className={styles.logoImage} src={logo} alt="logo" />
         </Link>
       </div>
-      <h1 className={styles.title}>前端九部</h1>
+      <h2 className={styles.title}>前端九部工具市场</h2>
     </div>
   </Header>
 )
