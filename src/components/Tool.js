a4cab0cb7bf821287b58be035f4c243b371c3409
fkysly <fkysly@gmail.com>
2018-11-15 12:07:27 +0800

    adjust card

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -4,16 +4,17 @@ import styles from '../styles/components/Tool.module.less'
 
 const { Meta } = Card;
 
-const Tool = ({ title }) => (
+const Tool = ({ title, description }) => (
   <Card
     className={styles.tool}
-    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+    hoverable={true}
+    bordered={false}
   >
     <Meta
       avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
       title={title}
-      description="This is the description"
+      description={description}
+      className={styles.meta}
     />
   </Card>
 )
--- src/components/ToolList.js
+++ src/components/ToolList.js
@@ -5,26 +5,44 @@ import Tool from './Tool'
 
 const data = [
   {
-    title: 'Title 1',
+    id: 0,
+    title: 'Base64 编解码',
+    description: '提供对 Base 64 编码和解码的支持提供对 Base 64 编码和解码的支持提供对 Base 64 编码和解码的支持提供对 Base 64 编码和解码的支持',
   },
   {
+    id: 1,
     title: 'Title 2',
+    description: 'desc2',
   },
   {
+    id: 2,
     title: 'Title 3',
+    description: 'desc3',
   },
   {
+    id: 3,
     title: 'Title 4',
+    description: 'desc4',
+  },
+  {
+    id: 4,
+    title: 'Title 45',
+    description: 'desc45',
+  },
+  {
+    id: 5,
+    title: 'Title 41',
+    description: 'desc41',
   },
 ];
 
 const ToolList = ({}) => (
   <List className={styles.toollist}
-    grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 3, xxl: 4 }}
+    grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 3, xxl: 3 }}
     dataSource={data}
     renderItem={item => (
-      <List.Item className={styles.toollistItem}>
-        <Tool title={item.title}></Tool>
+      <List.Item className={styles.toollistItem} key={item.id}>
+        <Tool {...item}></Tool>
       </List.Item>
     )}
   />
