f59b4ab3e9967ced609138e24e28de3cbe7f4f6d
fkysly <fkysly@gmail.com>
2018-11-22 13:25:50 +0800

    add base64 encode & decode

--- src/libs/Base64/Base64.js
+++ src/libs/Base64/Base64.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
-import base64js from 'base64-js'
 import { Row, Col, Button, Input } from 'antd'
 import styles from './Base64.module.less'
 
 const { TextArea } = Input
 
 class Base64 extends Component {
+  handleEncode = () => {
+    this.encodeInput.textAreaRef.value = 
+      Buffer.from(this.decodeInput.textAreaRef.value, 'base64').toString('ascii')
+  }
+
+  handleDecode = () => {
+    this.decodeInput.textAreaRef.value = 
+      Buffer.from(this.encodeInput.textAreaRef.value).toString('base64')
+  }
+
   render() {
     return (
       <div className={styles.container}>
         <Row type="flex" justify="center" align="middle">
           <Col xs={8} sm={9} md={8} lg={8} xl={8} xxl={8}>
-            <TextArea rows={10} className={styles.textarea} />
+            <TextArea placeholder="请输入要编码的内容" rows={10} className={styles.textarea} ref={(ref) => this.encodeInput = ref} />
           </Col>
           <Col xs={{ span: 8, offset: 0 }} 
                sm={{ span: 6, offset: 0 }}
@@ -19,11 +28,11 @@ class Base64 extends Component {
                lg={{ span: 5, offset: 0 }}
                xl={{ span: 4, offset: 0 }}
                xxl={{ span: 4, offset: 0 }}>
-            <Button className={styles.encode}>&#60;&#45; encode</Button>
-            <Button className={styles.decode}>decode &#45;&#62;</Button>
+            <Button className={styles.encode} onClick={this.handleEncode}>&#60;&#45; encode</Button>
+            <Button className={styles.decode} onClick={this.handleDecode}>decode &#45;&#62;</Button>
           </Col>
           <Col xs={8} sm={9} md={8} lg={8} xl={8} xxl={8}>
-            <TextArea rows={10} className={styles.textarea} />
+            <TextArea placeholder="请输入要解码的内容" rows={10} className={styles.textarea} ref={(ref) => this.decodeInput = ref} />
           </Col>
         </Row>
       </div>
